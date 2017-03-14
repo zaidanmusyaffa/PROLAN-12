5bc24db814d2c8c81f42f93a8f9371568bb7ace4
Muhammad Zaidan Musyaffa Z <rafialkazzam@gmail.com>
2017-03-14 23:26:01 +0700

    Belajar

---
+++ Minggu-3/Komputer.java
@@ -0,0 +1,12 @@
+public class Komputer {
+
+    
+    public static void main(String[] args) {
+        data asus = new data();
+        asus.monitor="Asus";
+        asus.keyboard="Genius";
+        asus.mouse="Logitech";
+        asus.info();
+    }
+    
+}
---
+++ Minggu-3/data.java
@@ -0,0 +1,10 @@
+public class data {
+    public String monitor,keyboard,mouse;
+    
+    public void info (){
+        System.out.println("Info:");
+        System.out.println("Monitor "+ monitor);
+        System.out.println("Keyboard "+ keyboard);
+        System.out.println("Mouse "+ mouse);
+    }
+}
